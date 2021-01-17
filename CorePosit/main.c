6ed7424407d2195c86a4062adb1d990e964dda82
Antoine Caillot <ant.kyo@gmail.com>
2021-01-17 20:32:24 +0100

    Initial Commit

---
+++ CorePosit/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CorePosit
+//
+//  Created by CAILLOT Antoine on 17/01/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
